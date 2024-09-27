430f07b97074115743a9cd3b6466d8aad453ad43
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-09-27 15:07:29 +0300

    Mapget app config

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddCookie();
 
 var app = builder.Build();
 
@@ -18,8 +27,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapGet("/", () => "Hello World! identity");
+app.MapGet("/protected", () => "secret!").RequireAuthorization();
+app.MapGet("/login", (HttpContext ctx) =>
+{
+    ctx.SignInAsync(new ClaimsPrincipal(new[]
+    {
+        new ClaimsIdentity(new List<Claim>()
+        {
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+        },
+        CookieAuthenticationDefaults.AuthenticationScheme)
+    }));
+    return "ok";
+});
 app.MapControllers();
 
 app.Run();
